d8db8eb1d605742f184802a3cc2ad6bc4e82d84d
niranjansd <niranjan.sd@gmail.com>
2022-07-18 21:44:49 -0700

    7

---
+++ 0007.py
@@ -0,0 +1,21 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+import numpy as np
+
+def SieveOfEratosthenes(n = 1000005):
+    prime = [True for i in range(n + 1)]
+     
+    p = 2
+    while (p * p <= n):
+        # If prime[p] is not changed,
+        # then it is a prime
+        if (prime[p] == True):
+            # Update all multiples of p
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+    return np.nonzero(prime)[0]
+
+primes = SieveOfEratosthenes(110000)
+print(primes[10002])
+# print(SieveOfEratosthenes(110000))
